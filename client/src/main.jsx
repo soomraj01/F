47f146619e0eb8f44f8ab5e159972812e5f2c17e
Soom Raj <soomraj01@gmail.com>
2026-09-23 19:26:10 +0530

    Add profile image favicon

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import profilePhoto from './assets/profile.png';
 import './styles.css';
+
+// This function sets the browser tab icon to the owner's profile photo.
+const favicon = document.querySelector('link[rel="icon"]') || document.createElement('link');
+favicon.rel = 'icon';
+favicon.type = 'image/png';
+favicon.href = profilePhoto;
+document.head.appendChild(favicon);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
